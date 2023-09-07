2305b6c052a520a7378a376dedcc33560be2edca
Santiago Contreras <santiagocoar@unisabana.edu.co>
2023-09-06 23:13:17 -0500

    Corregir método de búsqueda por ID de @RequestParam a @Pathvariable. Agregar filtro por categorias

--- src/main/java/co/edu/unisabana/inventario/Inventario/bd/ProductoRepository.java
+++ src/main/java/co/edu/unisabana/inventario/Inventario/bd/ProductoRepository.java
@@ -1,5 +1,9 @@
 package co.edu.unisabana.inventario.Inventario.bd;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto,Integer>{
+
 }
