0654f9afd677e99597e65c46ceb921ce1505ce21
Deutsch <juandeuto@gmail.com>
2023-09-27 12:48:16 -0500

    Limpia imports para optimizacion de codigo

--- src/test/java/co/edu/unisabana/inventario/Inventario/integracion/InventarioControllerTest.java
+++ src/test/java/co/edu/unisabana/inventario/Inventario/integracion/InventarioControllerTest.java
@@ -1,19 +1,19 @@
 package co.edu.unisabana.inventario.Inventario.integracion;
 
+import co.edu.unisabana.inventario.Inventario.bd.ProductoRepository;
 import co.edu.unisabana.inventario.Inventario.controlador.dto.ProductoDTO;
 import co.edu.unisabana.inventario.Inventario.controlador.dto.RespuestaDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.internal.matchers.Null;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,6 +22,9 @@ class InventarioControllerTest {
 
     @Autowired
     TestRestTemplate rest;
+
+    @Mock
+    ProductoRepository repository;
 
     @Test
     void agregarProductoTest() {
@@ -35,12 +38,14 @@ class InventarioControllerTest {
     void borrarProducto() {
     }
 
+
     @Test
     void actualizarProducto() {
     }
 
     @Test
     void verPorID() {
+        ProductoDTO dto = new ProductoDTO(7, "Guitarra", "Intrumento de cuerdas", 13, 15, "Instrumentos");
     }
 
     @Test
