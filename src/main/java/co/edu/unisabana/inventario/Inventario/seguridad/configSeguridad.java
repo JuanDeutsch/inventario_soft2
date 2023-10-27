f304bf0fc737d3068fbe7006f602c4c789ee588c
Santiago Contreras <santiagocoar@unisabana.edu.co>
2023-10-27 12:55:34 -0500

    Eliminar imports innecesarios

--- src/main/java/co/edu/unisabana/inventario/Inventario/seguridad/configSeguridad.java
+++ src/main/java/co/edu/unisabana/inventario/Inventario/seguridad/configSeguridad.java
@@ -1,12 +1,9 @@
 package co.edu.unisabana.inventario.Inventario.seguridad;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 @EnableWebSecurity
--- src/test/java/co/edu/unisabana/inventario/Inventario/integracion/InventarioControllerTest.java
+++ src/test/java/co/edu/unisabana/inventario/Inventario/integracion/InventarioControllerTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
